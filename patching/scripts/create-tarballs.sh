61818f6ff804023699bbedbb16576df99aa3ceda
Michael Snoyman <michael@snoyman.com>
2013-06-30 13:42:26 +0300

    Script fix

--- patching/scripts/create-tarballs.sh
+++ patching/scripts/create-tarballs.sh
@@ -9,13 +9,14 @@ do
     PKG=${PKG1%.patch}
     rm -rf tmp
     mkdir tmp
+    (
     cd tmp
     cabal unpack $PKG
     cd $PKG
     patch -p1 < ../../$f
     cabal sdist
     mv dist/$PKG.tar.gz ../../tarballs
-    cd ..
+    )
     rm -rf tmp
     )
 done
