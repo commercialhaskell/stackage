3cc5a3da3673da597e273a4cdcca907651e513d4
Michael Snoyman <michael@snoyman.com>
2013-09-01 11:43:29 +0300

    Upper bounds for #107

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -223,6 +223,9 @@ defaultStablePackages ghcVer = unPackageMap $ execWriter $ do
     addRange "Michael Snoyman" "fay" "< 0.16.0.2"
     addRange "Michael Snoyman" "diagrams-haddock" "< 0.1.1.2"
 
+    -- https://github.com/fpco/stackage/issues/107
+    addRange "Michael Snoyman" "split" "< 0.2.2"
+
     addRange "Michael Snoyman" "hashable" "< 1.2"
 
     -- Requires containers 0.5, not supported by GHC 7.4
