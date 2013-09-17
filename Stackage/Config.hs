908a8c201860677f2ba510d4e4ed40a0d58a71d7
Michael Snoyman <michael@snoyman.com>
2013-09-17 08:13:18 +0300

    Upper bounds for #116

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -245,6 +245,9 @@ defaultStablePackages ghcVer = unPackageMap $ execWriter $ do
     -- https://github.com/fpco/stackage/issues/115
     addRange "Michael Snoyman" "pandoc" "< 1.12"
 
+    -- https://github.com/fpco/stackage/issues/116
+    addRange "Michael Snoyman" "semigroups" "< 0.10"
+
     addRange "Michael Snoyman" "hashable" "< 1.2"
 
     -- unknown symbol `utf8_table4'
