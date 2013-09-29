074c99c325541c786ed80be565eff53b5d3b4f98
Michael Snoyman <michael@snoyman.com>
2013-09-29 10:44:04 +0300

    Remove upper bounds and close #119

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -251,11 +251,6 @@ defaultStablePackages ghcVer = unPackageMap $ execWriter $ do
     -- https://github.com/fpco/stackage/issues/118
     addRange "Michael Snoyman" "pem" "< 0.2"
 
-    -- https://github.com/fpco/stackage/issues/119
-    addRange "Michael Snoyman" "comonad" "< 3.1"
-    addRange "Michael Snoyman" "comonad-transformers" "< 3.1"
-    addRange "Michael Snoyman" "semigroupoids" "< 3.1"
-
     -- https://github.com/fpco/stackage/issues/121
     addRange "Michael Snoyman" "cereal" "< 0.4"
 
