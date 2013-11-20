ba5570464bbc704f3a4babb619e6e4e7774f8ebb
Michael Snoyman <michael@snoyman.com>
2013-11-20 09:01:50 +0200

    Remove upper bound and close #124

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -274,9 +274,6 @@ defaultStablePackages ghcVer = unPackageMap $ execWriter $ do
     -- https://github.com/fpco/stackage/issues/123
     addRange "Michael Snoyman" "cryptohash" "< 0.11"
 
-    -- https://github.com/fpco/stackage/issues/124
-    addRange "Michael Snoyman" "fclabels" "< 2.0"
-
     -- Newest hxt requires network 2.4 or newest
     addRange "Michael Snoyman" "hxt" "< 9.3.1"
     addRange "Michael Snoyman" "network" "< 2.4"
