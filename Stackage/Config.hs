a89ac24744baefc0644c6b075ca5b70b668141e3
Michael Snoyman <michael@snoyman.com>
2013-12-05 19:26:19 +0200

    Remove upper bounds and close #97

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -270,9 +270,6 @@ defaultStablePackages ghcVer = unPackageMap $ execWriter $ do
     -- Due to binary package dep
     addRange "Michael Snoyman" "statistics" "< 0.10.4"
 
-    -- https://github.com/fpco/stackage/issues/97
-    addRange "Michael Snoyman" "tagsoup" "< 0.13"
-
     -- Newest hxt requires network 2.4 or newest
     addRange "Michael Snoyman" "hxt" "< 9.3.1"
     addRange "Michael Snoyman" "network" "< 2.4"
