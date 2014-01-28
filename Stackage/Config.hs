463f32af888d1bdae0adfb4bb112f52d17ec9209
Michael Snoyman <michael@snoyman.com>
2014-01-28 06:24:16 +0200

    Remove upper bounds and close #176

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -341,9 +341,6 @@ defaultStablePackages ghcVer = unPackageMap $ execWriter $ do
     addRange "Michael Snoyman" "blaze-html" "< 0.7"
     addRange "Michael Snoyman" "blaze-markup" "< 0.6"
 
-    -- https://github.com/fpco/stackage/issues/176
-    addRange "Michael Snoyman" "diagrams-builder" "< 0.5"
-
     addRange "Michael Snoyman" "hashable" "< 1.2"
 
     -- Requires binary 0.7
