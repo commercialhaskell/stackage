fa3722b22ae6d3023da5bd80ecd6119b945ff7f1
Michael Snoyman <michael@snoyman.com>
2014-03-06 18:48:56 +0200

    Move conduit-combinators

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -144,6 +144,7 @@ defaultStablePackages ghcVer = unPackageMap $ execWriter $ do
         , "classy-prelude-yesod yesod-fay yesod-eventsource wai-websockets"
         , "random-shuffle safe-failure hackage-proxy hebrew-time"
         , "bzlib-conduit case-insensitive"
+        , "conduit-combinators"
         ]
     when (ghcVer >= GhcMajorVersion 7 6) $ add "michael@snoyman.com" "mega-sdist"
 #if !defined(mingw32_HOST_OS) && !defined(__MINGW32__)
@@ -160,7 +161,6 @@ defaultStablePackages ghcVer = unPackageMap $ execWriter $ do
         , "cairo diagrams-cairo"
         , "persistent-mongoDB fpco-api"
         , "threepenny-gui base16-bytestring convertible"
-        , "conduit-combinators"
         ]
     -- Deprecated version
     addRange "FP Complete <michael@fpcomplete.com>" "persistent-mongoDB" "< 1.3.1 || > 1.3.1"
