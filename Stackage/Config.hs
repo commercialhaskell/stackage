2d4b74f66984cdb246f7395c20afebdd8bbc097f
Michael Snoyman <michael@snoyman.com>
2014-03-25 16:12:06 +0200

    Cloud Haskell deps not up-to-date

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -169,8 +169,11 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
         , "cairo diagrams-cairo"
         , "persistent-mongoDB fpco-api"
         , "threepenny-gui base16-bytestring convertible"
-        , "distributed-process distributed-process-simplelocalnet"
         ]
+    when (ghcVer == GhcMajorVersion 7 4) $
+        mapM_ (add "FP Complete <michael@fpcomplete.com>") $ words =<<
+            [ "distributed-process distributed-process-simplelocalnet"
+            ]
     -- Deprecated version
     addRange "FP Complete <michael@fpcomplete.com>" "persistent-mongoDB" "< 1.3.1 || > 1.3.1"
     when (ghcVer < GhcMajorVersion 7 6) $ do
