22ebf28ddc09120d4bb2dbe9da03d93de7c6075d
Michael Snoyman <michael@snoyman.com>
2014-03-30 09:17:15 +0300

    Avoid csv-conduit 0.6.2 ozataman/csv-conduit#10

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -368,6 +368,9 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
     -- https://github.com/fpco/stackage/issues/204
     addRange "Michael Snoyman" "intervals" "< 0.5"
 
+    -- https://github.com/ozataman/csv-conduit/issues/10
+    addRange "Michael Snoyman" "csv-conduit" "< 0.6.2 || > 0.6.2"
+
     -- Requires too new a version of text
     when (ghcVer == GhcMajorVersion 7 4 && requireHP) $ do
         addRange "Michael snoyman" "attoparsec" "< 0.11.2.1"
