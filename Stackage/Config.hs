685f657e70048f3ea906b9fb96bd02c79a007d22
Michael Snoyman <michael@snoyman.com>
2014-05-16 09:07:16 +0300

    Remove upper bounds and close #222

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -404,9 +404,6 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
     -- https://github.com/fpco/stackage/issues/219
     addRange "Michael Snoyman" "comonad" "< 4.2"
 
-    -- https://github.com/fpco/stackage/issues/222
-    addRange "Michael Snoyman" "scientific" "< 0.3"
-
     -- Requires too new a version of text
     when (ghcVer == GhcMajorVersion 7 4 && requireHP) $ do
         addRange "Michael Snoyman" "attoparsec" "< 0.11.2.1"
