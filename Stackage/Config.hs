dd8b91c819d944fef5da483b1a2435509b1f0ad7
Phil Hargett <hargettp@users.noreply.github.com>
2014-05-22 17:11:11 -0700

    Added courier

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -336,6 +336,9 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
         , "haskell-names haskell-packages hse-cpp"
         ]
 
+    mapM_ (add "Phil Hargett <phil@haphazardhouse.net>") $ words
+        "courier"
+        
 #if !defined(mingw32_HOST_OS) && !defined(__MINGW32__)
     mapM_ (add "Aycan iRiCAN <iricanaycan@gmail.com>") $ words
         "hdaemonize hsyslog hweblib"
