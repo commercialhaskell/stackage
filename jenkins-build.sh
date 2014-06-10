ec656a912ce6bde5ba218f8880867379ec71180a
Michael Snoyman <michael@snoyman.com>
2014-06-10 05:22:46 +0300

    Jenkins: build right version of Cabal library

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -3,7 +3,7 @@
 cabal update
 (cd patching/ && ./scripts/create-tarballs.sh)
 cabal install
-cabal install Cabal
+cabal install Cabal-$(cabal --version | sed -n 's@using version \(.*\) of the Cabal library@\1@p')
 ./dist/build/stackage/stackage select --no-platform
 ./dist/build/stackage/stackage check
 ./dist/build/stackage/stackage build
