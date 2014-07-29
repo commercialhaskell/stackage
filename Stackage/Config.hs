a26c84a56f7ace959bafd7766bb750d0a3971f68
Michael Snoyman <michael@snoyman.com>
2014-07-29 06:54:30 +0300

    More upper bounds for #237

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -465,6 +465,9 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
 
     -- https://github.com/fpco/stackage/issues/237
     addRange "Michael Snoyman" "lens" "< 4.2"
+    addRange "Michael Snoyman" "contravariant" "< 1"
+    addRange "Michael Snoyman" "kan-extensions" "< 4.1"
+    addRange "Michael Snoyman" "adjunctions" "< 4.2"
 
     -- https://github.com/fpco/stackage/issues/242
     addRange "Michael Snoyman" "mongoDB" "< 1.5"
