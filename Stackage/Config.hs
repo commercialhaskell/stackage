0bb3e90361983ebcca54605aafe57606335fa9bc
Michael Snoyman <michael@snoyman.com>
2014-08-16 23:19:40 +0300

    Add hmatrix and hmatrix-gsl

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -223,7 +223,7 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
         , "base16-bytestring convertible"
         , "compdata hybrid-vectors"
         , "executable-path formatting quandl-api"
-        , "fgl"
+        , "fgl hmatrix hmatrix-gsl"
         ]
     when (ghcVer < GhcMajorVersion 7 8) $ do -- No GHC 7.8 support
         mapM_ (add "FP Complete <michael@fpcomplete.com>") $ words =<<
