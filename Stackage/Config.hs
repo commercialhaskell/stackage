b09b1a2b4009e505d060828e1b09dc7b76a19465
Michael Snoyman <michael@snoyman.com>
2014-09-19 07:42:40 +0300

    Add back esqueleto

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -345,6 +345,8 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
 
     mapM_ (add "Patrick Brisbin") $ words "gravatar"
 
+    mapM_ (add "Felipe Lessa <felipe.lessa@gmail.com>") $ words
+        "esqueleto"
     {- persistent 2.0/conduit 1.2
     mapM_ (add "Felipe Lessa <felipe.lessa@gmail.com>") $ words
         "esqueleto fb fb-persistent yesod-fb yesod-auth-fb"
