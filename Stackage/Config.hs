85a58e06ed338695ce5d5c193c25e025147df8e5
Neil Mitchell <ndmitchell@gmail.com>
2014-10-06 23:41:53 +0100

    Add some new packages by Neil

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -280,7 +280,7 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
     addRange "FP Complete <michael@fpcomplete.com>" "kure" "<= 2.4.10"
 
     mapM_ (add "Neil Mitchell") $ words
-        "hlint hoogle shake derive tagsoup cmdargs safe uniplate nsis"
+        "hlint hoogle shake derive tagsoup cmdargs safe uniplate nsis js-jquery extra bake"
 
     mapM_ (add "Alan Zimmerman") $ words
         "hjsmin language-javascript"
