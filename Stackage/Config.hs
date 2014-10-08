cc861534e75664f91e6cf8a59ace5eac1141239d
Boris <boris.buliga@icloud.com>
2014-10-08 12:26:42 +0300

    Add ghc-mod and it's dependencies

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -477,6 +477,9 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
 
     mapM_ (add "Thomas Schilling <nominolo@googlemail.com>") $ words
         "ghc-syb-utils"
+        
+    mapM_ (add "Boris Buliga <d12frosted@icloud.com>") $ words
+        "djinn-lib djinn-ghc ghc-mod io-choice"
 
     when (ghcVer >= GhcMajorVersion 7 8) $
         mapM_ (add "Yann Esposito <yann.esposito@gmail.com>") $ words
