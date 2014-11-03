7828e75d3458af37277dc4984c3ea6a1eff05306
Michael Snoyman <michael@snoyman.com>
2014-11-03 13:25:52 +0200

    Remove upper bounds for liyang/thyme#29

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -540,10 +540,6 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
     addRange "Michael Snoyman" "mtl" "< 2.2"
     addRange "Michael Snoyman" "lifted-base" "< 0.2.2.2"
 
-    -- https://github.com/liyang/thyme/issues/29
-    when (ghcVer <= GhcMajorVersion 7 6) $
-        addRange "Michael Snoyman" "thyme" "< 0.3.5.3"
-
     -- https://github.com/fpco/stackage/issues/224
     when (ghcVer <= GhcMajorVersion 7 6) $ do
         addRange "Michael Snoyman" "zip-archive" "== 0.2.2.1"
