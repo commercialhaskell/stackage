dd5f2171c3bb4676b842b8a92db1f7e25bfd0c7b
Michael Snoyman <michael@snoyman.com>
2014-11-10 22:42:05 +0200

    Upper bounds for #326

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -598,6 +598,9 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
     -- https://github.com/fpco/stackage/issues/320
     addRange "Michael Snoyman" "lens" "< 4.5"
 
+    -- https://github.com/fpco/stackage/issues/326
+    addRange "Michael Snoyman" "hspec" "< 2"
+
     when (ghcVer == GhcMajorVersion 7 8 && requireHP) $ do
         -- Yay workarounds for unnecessarily old versions
         let peg x y = addRange "Haskell Platform" x y
