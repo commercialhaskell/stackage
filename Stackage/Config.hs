2908bfe255c976c47aaa1e5bd14f2057a64498b8
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-11-13 15:33:19 +0300

    More packages

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -535,7 +535,7 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
 
     mapM_ (add "Nikita Volkov <nikita.y.volkov@mail.ru>") $ 
         words "hasql hasql-postgres hasql-backend postgresql-binary" ++
-        words "stm-containers slave-thread partial-handler" ++
+        words "stm-containers focus list-t slave-thread partial-handler" ++
         words "base-prelude mtl-prelude"
 
     -- https://github.com/fpco/stackage/issues/216
