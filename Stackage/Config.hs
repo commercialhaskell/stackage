94f8532b4bf75f9aca8d57b0f95e02ab2514bf85
Michael Snoyman <michael@snoyman.com>
2014-11-30 14:41:14 +0200

    Add scrobble back in chrisdone/scrobble#1

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -341,11 +341,9 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
     mapM_ (add "Chris Done") $ words =<<
         [ "ace check-email freenect gd"
         , "hostname-validate ini lucid osdkeys pdfinfo"
-        , "pure-io sourcemap"
+        , "pure-io sourcemap scrobble"
         -- https://github.com/nominolo/atto-lisp/issues/15
         -- , "present"
-        -- https://github.com/chrisdone/scrobble/issues/1
-        -- , "scrobble"
         -- https://github.com/fpco/stackage/issues/344
         -- , "frisby"
         ]
