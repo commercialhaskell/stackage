e360a857cde4dd13666a01cbeab111bb26280e3d
Michael Snoyman <michael@snoyman.com>
2014-12-04 14:34:15 +0200

    PackageConstraints rename

--- Stackage2/PackageConstraints.hs
+++ Stackage2/PackageConstraints.hs
@@ -1,30 +1,30 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
--- | Get the proposed build plan.
-module Stackage2.ProposedPlan
-    ( ProposedPlan (..)
-    , defaultProposedPlan
+-- | The constraints on package selection for a new build plan.
+module Stackage2.PackageConstraints
+    ( PackageConstraints (..)
+    , defaultPackageConstraints
     ) where
 
 import           Stackage2.Prelude
 import qualified Stackage.Config as Old
 import qualified Stackage.Types  as Old
 
-data ProposedPlan = ProposedPlan
-    { ppPackages :: Map PackageName (VersionRange, Maintainer)
+data PackageConstraints = PackageConstraints
+    { pcPackages :: Map PackageName (VersionRange, Maintainer)
     -- ^ This does not include core packages or dependencies, just packages
     -- added by some maintainer.
-    , ppExpectedFailures :: Set PackageName
+    , pcExpectedFailures :: Set PackageName
     -- ^ At some point in the future, we should split this into Haddock
     -- failures, test failures, etc.
     }
 
 -- | The proposed plan from the requirements provided by contributors.
-defaultProposedPlan :: ProposedPlan
-defaultProposedPlan = ProposedPlan
-    { ppPackages = fmap (Maintainer . pack . Old.unMaintainer)
+defaultPackageConstraints :: PackageConstraints
+defaultPackageConstraints = PackageConstraints
+    { pcPackages = fmap (Maintainer . pack . Old.unMaintainer)
                <$> Old.defaultStablePackages ghcVer False
-    , ppExpectedFailures = Old.defaultExpectedFailures ghcVer False
+    , pcExpectedFailures = Old.defaultExpectedFailures ghcVer False
     }
   where
     ghcVer = Old.GhcMajorVersion 7 8
