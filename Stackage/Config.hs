9e4231c5a020887b090397dc564d11ace786907d
Joey Eremondi <jse313@mail.usask.ca>
2014-12-13 14:17:40 +0100

    Update Config.hs

--- Stackage/Config.hs
+++ Stackage/Config.hs
@@ -652,9 +652,10 @@ defaultStablePackages ghcVer requireHP = unPackageMap $ execWriter $ do
     mapM_ (add "Alexander Thiemann <mail@athiemann.net>") $ words
        "graph-core reroute Spock"
        
-    mapM_ (add "Joey Eremondi <joey@eremondi.com>") $ 
-       words "prettyclass language-glsl union-find aeson-pretty QuasiText" ++
-       words "digest zip-archive elm-compiler elm-package elm-core-sources elm-build-lib" 
+    mapM_ (add "Joey Eremondi <joey@eremondi.com>") $ words =<<
+        [ "prettyclass language-glsl union-find aeson-pretty QuasiText"
+        , "digest zip-archive elm-compiler elm-package elm-core-sources elm-build-lib"
+        ]
 
     -- https://github.com/fpco/stackage/issues/217
     addRange "Michael Snoyman" "transformers" "< 0.4"
