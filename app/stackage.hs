f51b86e165f109930e462f110bc0155215824e47
Emanuel Borsboom <manny@fpcomplete.com>
2015-01-29 14:03:54 -0800

    Add `install` subcommand.
    Used to install a Stackage snapshot from a build plan.

--- app/stackage.hs
+++ app/stackage.hs
@@ -7,8 +7,10 @@ import Data.Monoid
 import Data.String (fromString)
 import Data.Version
 import Options.Applicative
+import Filesystem.Path.CurrentOS (decodeString)
 import Paths_stackage (version)
 import Stackage.CompleteBuild
+import Stackage.InstallBuild
 
 main :: IO ()
 main =
@@ -25,9 +27,9 @@ main =
         help "Show this help text"
     versionOption =
         infoOption
-            ("fpbuild version " ++ showVersion version)
+            ("stackage version " ++ showVersion version)
             (long "version" <>
-             help "Show fpbuild version")
+             help "Show stackage version")
     config =
         subparser $
         mconcat
@@ -55,12 +57,19 @@ main =
                   (const justCheck)
                   (pure ())
                   "check"
-                  "Just check that the build plan is ok"]
+                  "Just check that the build plan is ok"
+            , cmnd
+                  installBuild
+                  installFlags
+                  "install"
+                  "Install a snapshot from an existing build plan"]
+
     cmnd exec parse name desc =
         command name $
         info
-            (fmap exec parse)
+            (fmap exec (parse <**> helpOption))
             (progDesc desc)
+
     buildFlags =
         BuildFlags <$>
         fmap
@@ -86,3 +95,53 @@ main =
     nightlyUploadFlags = fromString <$> strArgument
         (metavar "DATE" <>
          help "Date, in YYYY-MM-DD format")
+
+    installFlags =
+        InstallFlags <$>
+        (fmap
+            BPSBundleWeb
+            (strOption
+                (long "bundle" <>
+                 metavar "URL" <>
+                 help "Stackage bundle containing build plan")) <|>
+         fmap
+            (BPSFile . decodeString)
+            (strOption
+                (long "build-plan" <>
+                 metavar "PATH" <>
+                 help "Build-plan YAML file"))) <*>
+        fmap
+            decodeString
+            (strArgument
+                (metavar "DESTINATION-PATH" <>
+                 help "Destination directory path")) <*>
+        (fmap
+            (Just . decodeString)
+            (strOption
+                (long "log-dir" <>
+                 metavar "PATH" <>
+                 help "Location of log files (default DESTINATION-PATH/logs)")) <|>
+         pure Nothing) <*>
+        option
+            auto
+            (long "jobs" <>
+             metavar "NUMBER" <>
+             showDefault <> value 8 <>
+             help "Number of threads") <*>
+        switch
+            (long "global" <>
+             help "Install in global package database") <*>
+        fmap
+            not
+            (switch
+                (long "skip-tests" <>
+                 help "Skip build and running the test suites")) <*>
+        switch
+            (long "enable-library-profiling" <>
+             help "Enable profiling when building") <*>
+        switch
+            (long "verbose" <> short 'v' <>
+             help "Output verbose detail about the build steps") <*>
+        switch
+            (long "skip-check" <>
+             help "Skip the check phase, and pass --allow-newer to cabal configure")
