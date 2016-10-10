c2170ddcea056efff791c76b2c54921fa93e1afd
Jens Petersen <juhpetersen@gmail.com>
2016-10-10 12:37:16 +0900

    run-nightly.sh: remove newline before cron.sh
    I keep Ctrl-c'ing cron.sh by mistake, thinking it has already finished

--- automated/run-nightly.sh
+++ automated/run-nightly.sh
@@ -4,7 +4,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 while true; do
     ./build.sh nightly-$(date -u +%F)
-    echo
 
     echo "Running cron.sh (hiding verbose output)"
     ./cron.sh > cron.log 2>&1 # | grep -v '^Skipping'
