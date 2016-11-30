12a894ccdd4d5677d2b42082bbe5b5300920d294
Michael Snoyman <michael@snoyman.com>
2016-11-30 16:05:42 +0200

    cron.sh: pull the image

--- automated/cron.sh
+++ automated/cron.sh
@@ -6,10 +6,13 @@ CRONDIR=$(pwd)/crondir
 mkdir -p $CRONDIR
 source aws.sh
 
+IMAGE=fpco/stackage-server-prod:latest
+docker pull $IMAGE
+
 echo "Running stackage-server-cron..."
 echo "('tail -f $CRONDIR/stackage-server-cron.log' to watch)"
 docker run --rm \
     -v $CRONDIR:/home/ubuntu \
     -w /home/ubuntu \
-    fpco/stackage-server-prod:latest \
+    $IMAGE \
     bash -c "useradd $(whoami) -u $(id -u); sudo -u $(whoami) env HOME=/home/ubuntu AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY bash -c '/usr/local/bin/stackage-server-cron 2>&1 | tee -a /home/ubuntu/stackage-server-cron.log'"
