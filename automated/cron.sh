287bbcea0e210d83ec210112c20408b2dfc97c42
Michael Snoyman <michael@snoyman.com>
2016-11-30 19:15:56 +0200

    Provide ~/.stack to cron job

--- automated/cron.sh
+++ automated/cron.sh
@@ -9,10 +9,12 @@ source aws.sh
 IMAGE=fpco/stackage-server-prod:latest
 docker pull $IMAGE
 
+stack update
+
 echo "Running stackage-server-cron..."
 echo "('tail -f $CRONDIR/stackage-server-cron.log' to watch)"
 docker run --rm \
     -v $CRONDIR:/home/ubuntu \
-    -w /home/ubuntu \
+    -v $HOME/.stack:/home/ubuntu/.stack:ro \
     $IMAGE \
     bash -c "useradd $(whoami) -u $(id -u); sudo -u $(whoami) env HOME=/home/ubuntu AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY bash -c '/usr/local/bin/stackage-server-cron 2>&1 | tee -a /home/ubuntu/stackage-server-cron.log'"
