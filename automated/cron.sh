acbdd495b807952d4bf55193157287a6fb969701
Stackage Curators <michael@snoyman.com>
2016-12-02 04:52:27 +0000

    Fix a buggy shell script

--- automated/cron.sh
+++ automated/cron.sh
@@ -15,6 +15,7 @@ echo "Running stackage-server-cron..."
 echo "('tail -f $CRONDIR/stackage-server-cron.log' to watch)"
 docker run --rm \
     -v $CRONDIR:/home/ubuntu \
-    -v $HOME/.stack:/home/ubuntu/.stack:ro \
+    -v $HOME/.stack/indices:/home/ubuntu/.stack/indices:ro \
+    --workdir /home/ubuntu \
     $IMAGE \
     bash -c "useradd $(whoami) -u $(id -u); sudo -u $(whoami) env HOME=/home/ubuntu AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY bash -c '/usr/local/bin/stackage-server-cron 2>&1 | tee -a /home/ubuntu/stackage-server-cron.log'"
