37b14ac86085b01d245202c703ef822f68f80deb
Jens Petersen <juhpetersen@gmail.com>
2016-12-06 16:18:54 +0900

    run-nightly.sh: run cron.sh in the background
    Check that cron.sh is not already running.
    With no new snapshot cron.sh takes about 1min.

--- automated/run-nightly.sh
+++ automated/run-nightly.sh
@@ -4,11 +4,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 while true; do
     ./build.sh nightly-$(date -u +%F)
-
-    date
-    echo -n "Running cron.sh (do not interrupt)... "
-    ./cron.sh > cron.log 2>&1
-    echo "done."
+    if ! pgrep cron.sh >/dev/null; then
+        echo
+        echo "Running cron.sh in the background"
+        ./cron.sh > cron.log 2>&1 &
+    fi
     echo
     date
 
