b01d84974e496aacd4224fdf9bc3ef0351099339
Michael Snoyman <michael@snoyman.com>
2016-12-06 09:50:13 -0500

    Stop running cron from build scripts

--- automated/run-nightly.sh
+++ automated/run-nightly.sh
@@ -4,12 +4,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 while true; do
     ./build.sh nightly-$(date -u +%F)
-    if ! pgrep cron.sh >/dev/null; then
-        echo
-        echo "Running cron.sh in the background"
-        ./cron.sh > cron.log 2>&1 &
-    fi
-    echo
     date
 
     sleep 30m
