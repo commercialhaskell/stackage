8b77287ca607d816994666af600fe8f137de13e0
Michael Snoyman <michael@snoyman.com>
2016-12-06 09:48:22 -0500

    Bind port in cron.sh

--- automated/cron.sh
+++ automated/cron.sh
@@ -17,5 +17,6 @@ docker run --rm \
     -v $CRONDIR:/home/ubuntu \
     -v $HOME/.stack/indices:/home/ubuntu/.stack/indices:ro \
     --workdir /home/ubuntu \
+    -p 17834:17834 \
     $IMAGE \
     bash -c "useradd $(whoami) -u $(id -u); sudo -u $(whoami) env HOME=/home/ubuntu AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY bash -c '/usr/local/bin/stackage-server-cron 2>&1 | tee -a /home/ubuntu/stackage-server-cron.log'"
