ded6e07ceef9a8b24955ade17fe9464db283caae
Adam Bergmark <adam@bergmark.nl>
2018-02-28 21:36:12 +0100

    debian-bootstrap: Add datadog env vars

--- debian-bootstrap.sh
+++ debian-bootstrap.sh
@@ -243,3 +243,7 @@ pushd /tmp \
 apt-add-repository multiverse \
     && apt-get update \
     && apt-get install -y nvidia-cuda-dev
+
+# For the datadog package
+DATADOG_API_KEY=abd7b2a270e44c0a846c94e44534b215
+DATADOG_APP_KEY=1775440c64d5bff48441a33755e072c82c2ab88b
