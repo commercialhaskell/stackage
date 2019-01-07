21c4abdf050351fad35676bf755b363d5fa7c721
Jens Petersen <juhpetersen@gmail.com>
2019-01-07 15:57:24 +0900

    debian-bootstrap.sh: rrutter seems unused

--- debian-bootstrap.sh
+++ debian-bootstrap.sh
@@ -19,7 +19,6 @@ apt-get update
 apt-get install -y software-properties-common
 
 add-apt-repository ppa:hvr/ghc -y
-add-apt-repository -y ppa:marutter/rrutter
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 add-apt-repository -y --keyserver hkp://keyserver.ubuntu.com:80 'deb http://download.mono-project.com/repo/debian wheezy main'
 add-apt-repository -y --keyserver hkp://keyserver.ubuntu.com:80 'deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main'
