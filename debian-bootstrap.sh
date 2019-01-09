0c5746221841985616fad024c808bf48cc21bf71
Michael Snoyman <michael@snoyman.com>
2019-01-09 14:58:14 +0200

    Put protoc in /usr/bin/protoc instead

--- debian-bootstrap.sh
+++ debian-bootstrap.sh
@@ -265,5 +265,5 @@ ldconfig
 # Instructions from: https://google.github.io/proto-lens/installing-protoc.html
 PROTOC_ZIP=protoc-3.3.0-linux-x86_64.zip
 curl -OL https://github.com/google/protobuf/releases/download/v3.3.0/$PROTOC_ZIP
-sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+sudo unzip -o $PROTOC_ZIP -d /usr bin/protoc
 rm -f $PROTOC_ZIP
