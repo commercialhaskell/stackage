7a28224cb1bd3038b19d0d72f0c7ddbc8397137d
Michael Snoyman <michael@snoyman.com>
2020-04-16 08:39:33 +0300

    Install AWS CLI

--- debian-bootstrap.sh
+++ debian-bootstrap.sh
@@ -17,9 +17,19 @@ mkdir -p /home/stackage
 export LANG=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 
-# Get curl
+# Get curl and unzip
 apt-get update
-apt-get install -y curl
+apt-get install -y curl unzip
+
+# Install AWS CLI
+mkdir -p /tmp/awscli
+(
+cd /tmp/awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+)
+rm -rf /tmp/awscli
 
 # Get Stack and GHC
 curl -sSL https://get.haskellstack.org/ | sh -s - -d /usr/bin
