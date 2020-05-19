ca686bacd18c2a95a314a23b95e0a708ca42aba8
Jens Petersen <juhpetersen@gmail.com>
2020-05-19 15:24:13 +0800

    clean-old-stack-libs.sh: pushd to show dir and run stack script
    - bail if error
    - make shell script executable instead

--- etc/diskspace/clean-old-stack-libs.sh
+++ etc/diskspace/clean-old-stack-libs.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+set -e
+
 if [ $# != 1 ]; then
     echo "Usage: $0 [nightly|lts-xx]"
     exit 1
 fi
 
-popd ~/stackage/automated/work/$1/unpack-dir/.stack-work/install/x86_64-linux/*/*/lib/x86_64-linux-ghc-*
+SRCDIR=$(dirname $0)
 
-~/stackage/etc/diskspace/remove-old-stack-work-libs.hs
+pushd ~/stackage/automated/work/$1/unpack-dir/.stack-work/install/x86_64-linux/*/*/lib/x86_64-linux-ghc-*
+
+stack --resolver lts-14 script ${SRCDIR}/remove-old-stack-work-libs.hs
