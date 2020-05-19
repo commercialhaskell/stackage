120b5380e01beb6ff40fd773c97b578a3e91f48e
Jens Petersen <juhpetersen@gmail.com>
2020-05-19 15:31:42 +0800

    clean-old-stack-libs.sh: don't be clever with relpaths

--- etc/diskspace/clean-old-stack-libs.sh
+++ etc/diskspace/clean-old-stack-libs.sh
@@ -7,8 +7,6 @@ if [ $# != 1 ]; then
     exit 1
 fi
 
-SRCDIR=$(dirname $0)
-
 pushd ~/stackage/automated/work/$1/unpack-dir/.stack-work/install/x86_64-linux/*/*/lib/x86_64-linux-ghc-*
 
-stack --resolver lts-14 script ${SRCDIR}/remove-old-stack-work-libs.hs
+stack --resolver lts-14 script ~/stackage/etc/diskspace/remove-old-stack-work-libs.hs
