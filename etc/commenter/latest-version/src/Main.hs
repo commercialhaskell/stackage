a1bea1cbe25859b6425d1202b790e7cac61da694
Adam Bergmark <adam@bergmark.nl>
2022-01-15 04:00:36 +0100

    commenter outdated: perf & warn on unversioned packages

--- etc/commenter/latest-version/src/Main.hs
+++ etc/commenter/latest-version/src/Main.hs
@@ -9,10 +9,20 @@ import System.Environment
 import qualified Data.Map as Map
 
 main :: IO ()
-main =
-  runPantryApp $
-    liftIO . putStrLn
-    . intercalate "." . map show . versionNumbers
-    . fst . head . Map.toDescList
-    =<< getHackagePackageVersions YesRequireHackageIndex IgnorePreferredVersions
-    . mkPackageName =<< head <$> liftIO getArgs
+main = do
+  args <- getArgs
+  (onlyVersion, packages) <- pure $ case args of
+    "only-version" : packages -> (True, packages)
+    packages -> (False, packages)
+  runPantryApp $ liftIO . putStrLn . unlines_ =<< mapM (latestVersion onlyVersion) packages
+  where
+    -- unlines adds an extra trailing newline which can be annoying...
+    unlines_ = intercalate "\n"
+
+latestVersion :: (HasPantryConfig env, HasLogFunc env) => Bool -> String -> RIO env String
+latestVersion onlyVersion name = fmap (displayVersion onlyVersion) . getVersion . mkPackageName $ name
+  where
+    showVersion = intercalate "." . map show . versionNumbers . fst . head . Map.toDescList
+    getVersion = getHackagePackageVersions YesRequireHackageIndex UsePreferredVersions
+    displayVersion True v = showVersion v
+    displayVersion False v = name <> "-" <> showVersion v
