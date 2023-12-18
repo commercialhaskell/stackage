6fb2885d7190e888429e89b0f9b0432748d163fa
Adam Bergmark <adam@bergmark.nl>
2023-12-18 13:17:08 +0100

    Run against LTS 22.0

--- etc/lts-constraints/src/Main.hs
+++ etc/lts-constraints/src/Main.hs
@@ -29,7 +29,7 @@ data State
 
 main :: IO ()
 main = do
-  map <- snapshotMap <$> loadSnapshot "../../../stackage-snapshots/nightly/2023/12/15.yaml"
+  map <- snapshotMap <$> loadSnapshot "../../../stackage-snapshots/lts/22/0.yaml"
   output <- openFile target WriteMode
   let putLine = liftIO . T.hPutStrLn output
   lines <- T.lines <$> T.readFile src
