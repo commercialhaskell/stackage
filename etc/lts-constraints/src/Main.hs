d49e1b107a454c22463cce0a306ef54eb1d00202
Adam Bergmark <adam@bergmark.nl>
2023-12-22 14:40:41 +0100

    Version LTS file

--- etc/lts-constraints/src/Main.hs
+++ etc/lts-constraints/src/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS -Wno-name-shadowing #-}
 module Main (main) where
 
@@ -11,6 +12,8 @@ import RIO.Map (Map)
 import System.IO (openFile, IOMode (..), hFlush, hClose)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Safe (at)
+import System.Environment (getArgs)
 
 import BuildConstraints (parsePackageDecl, handlePackage)
 import Snapshot (snapshotMap, loadSnapshot)
@@ -19,8 +22,8 @@ import Types (PackageName, Version)
 src :: String
 src = "../../build-constraints.yaml"
 
-target :: String
-target = "../../lts-build-constraints.yaml"
+target :: Int -> String
+target major = "../../lts-" <> show major <> "-build-constraints.yaml"
 
 data State
   = LookingForLibBounds
@@ -29,14 +32,18 @@ data State
 
 main :: IO ()
 main = do
-  map <- snapshotMap <$> loadSnapshot "../../../stackage-snapshots/lts/22/0.yaml"
-  output <- openFile target WriteMode
+  args :: [String] <- getArgs
+  print args
+  let major :: Int = read . (`at` 0) $ args
+  map <- snapshotMap <$> loadSnapshot ("../../../stackage-snapshots/lts/" <> show major <> "/0.yaml")
+  output <- openFile (target major) WriteMode
   let putLine = liftIO . T.hPutStrLn output
   lines <- T.lines <$> T.readFile src
   void $ flip runStateT LookingForLibBounds $ do
     forM_ lines $ putLine <=< processLine map
   hFlush output
   hClose output
+  putStrLn $ "Done. Wrote to " <> (target major)
 
 processLine :: MonadState State m => Map PackageName Version -> Text -> m Text
 processLine map line = do
