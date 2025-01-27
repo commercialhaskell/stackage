5ce2ac49542c6511542abb195039f19490a86038
Jens Petersen <juhpetersen@gmail.com>
2025-01-27 18:55:08 +0800

    enable gi-gtksource5, gi-javascriptcore6, gi-soup3 (haskell-gi/haskell-gi#463)
    metapackages remain disabled for now

--- docker/02-apt-get-install.sh
+++ docker/02-apt-get-install.sh
@@ -63,14 +63,14 @@ apt-get install -y \
     libgtk-3-dev \
     libgtk-4-dev \
     libgtk2.0-dev \
-    libgtksourceview-3.0-dev \
+    libgtksourceview-5-dev \
     libhidapi-dev \
     libi2c-dev \
     libicu-dev \
     libimlib2-dev \
     libjack-jackd2-dev \
     libjansson-dev \
-    libjavascriptcoregtk-4.0-dev \
+    libjavascriptcoregtk-6.0-dev \
     libjudy-dev \
     liblapack-dev \
     libleveldb-dev \
@@ -112,7 +112,7 @@ apt-get install -y \
     libsnappy-dev \
     libsndfile1-dev \
     libsodium-dev \
-    libsoup2.4-dev \
+    libsoup-3.0-dev \
     libsox-dev \
     libsqlite3-dev \
     libssl-dev \
