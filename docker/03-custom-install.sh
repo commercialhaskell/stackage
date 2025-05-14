18256dc2d5bc96bd41d5e1dd3e12dbd8d6ac5a73
Andreas Ländle <969523+alaendle@users.noreply.github.com>
2025-05-14 13:13:47 +0200

    Hope `proto-lens-protobuf-types` just needs up-to-date `protoc`.

--- docker/03-custom-install.sh
+++ docker/03-custom-install.sh
@@ -28,8 +28,8 @@ pushd /tmp \
 
 # protoc, for proto-lens-combinators test suite
 # Instructions from: https://google.github.io/proto-lens/installing-protoc.html
-PROTOC_ZIP=protoc-3.3.0-linux-x86_64.zip
-curl -OL https://github.com/google/protobuf/releases/download/v3.3.0/$PROTOC_ZIP
+PROTOC_ZIP=protoc-28.0-linux-x86_64.zip
+curl -OL https://github.com/google/protobuf/releases/download/v28.0/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr bin/protoc
 rm -f $PROTOC_ZIP
 
