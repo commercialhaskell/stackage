3421c957ffaecd0b74fd9e45dba80382a529da58
Jens Petersen <juhpetersen@gmail.com>
2026-02-18 15:28:51 +0800

    automated: add back .log extensions to log files

--- automated/run-lts.sh
+++ automated/run-lts.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 git pull
-time script -q -c "./build.sh $*" $1-build-$(date -u +%F_%T)
+time script -q -c "./build.sh $*" $1-build-$(date -u +%F_%T).log
 date
--- automated/run-nightly.sh
+++ automated/run-nightly.sh
@@ -9,7 +9,7 @@ esac
 
 while true; do
     git pull
-    time script -q -c "./build.sh nightly-$(date -u +%F) $cmd" nightly-build-$(date -u +%F_%T)
+    time script -q -c "./build.sh nightly-$(date -u +%F) $cmd" nightly-build-$(date -u +%F_%T).log
     ${cmd:+exit 0}
     ${once:+date; exit 0}
     echo "$0: run completed at $(date)"
