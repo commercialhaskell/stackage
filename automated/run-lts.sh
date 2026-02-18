b072e9acbaa4d968c71375214808d44f0875ad03
Jens Petersen <juhpetersen@gmail.com>
2026-02-18 14:46:18 +0800

    automated: use script to log each run with date_time stamp

---
+++ automated/run-lts.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+git pull
+time script -q -c "./build.sh $*" $1-build-$(date -u +%F_%T)
+date
--- automated/run-nightly.sh
+++ automated/run-nightly.sh
@@ -9,11 +9,11 @@ esac
 
 while true; do
     git pull
-    time ./build.sh nightly-$(date -u +%F) $cmd
+    time script -q -c "./build.sh nightly-$(date -u +%F) $cmd" nightly-build-$(date -u +%F_%T)
     ${cmd:+exit 0}
     ${once:+date; exit 0}
     echo "$0: run completed at $(date)"
     echo
-    sleep 60m
+    sleep 99m
     echo
 done
