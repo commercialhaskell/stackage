2b818000cfa592cc87e677f506f3cb111bc66a9d
Jens Petersen <juhpetersen@gmail.com>
2026-02-18 21:32:03 +0800

    automated: use + separator for date+time
    I keep seeing day_hour as hour_min

--- automated/run-lts.sh
+++ automated/run-lts.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 git pull
-LOG_FILE="$1-build-$(date -u +%F_%T).log"
+LOG_FILE="$1-build-$(date -u +%F+%T).log"
 ln -sf $LOG_FILE lts-build.log
 time script -q -c "./build.sh $*" $LOG_FILE
 touch -h lts-build.log
