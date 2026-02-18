129379baac1fe40a1660fa1b84b98ef080a46899
Jens Petersen <juhpetersen@gmail.com>
2026-02-18 20:11:20 +0800

    automated: simplify log filenames dropping "-latest"

--- automated/run-lts.sh
+++ automated/run-lts.sh
@@ -4,6 +4,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 git pull
 LOG_FILE="$1-build-$(date -u +%F_%T).log"
-ln -sf $LOG_FILE lts-build-last.log
+ln -sf $LOG_FILE lts-build.log
 time script -q -c "./build.sh $*" $LOG_FILE
 date
--- automated/run-nightly.sh
+++ automated/run-nightly.sh
@@ -10,10 +10,10 @@ esac
 while true; do
     git pull
     LOG_FILE="nightly-build-$(date -u +%F_%T).log"
-    if [ -L nightly-build-last.log ]; then
-        mv -f nightly-build-last.log nightly-build-previous.log
+    if [ -L nightly-build.log ]; then
+        mv -f nightly-build.log nightly-build-previous.log
     fi
-    ln -sf $LOG_FILE nightly-build-last.log
+    ln -sf $LOG_FILE nightly-build.log
     time script -q -c "./build.sh nightly-$(date -u +%F) $cmd" $LOG_FILE
     ${cmd:+exit 0}
     ${once:+date; exit 0}
