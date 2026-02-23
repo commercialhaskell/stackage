d61dc09ae63bf78dc63129636f5c5f49b2e6d2cf
Jens Petersen <juhpetersen@gmail.com>
2026-02-23 17:33:43 +0800

    automated/run-lts.sh: sync run-nightly tweaks
    [skip ci]

--- automated/run-lts.sh
+++ automated/run-lts.sh
@@ -4,7 +4,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 git pull
 LOG_FILE="logs/$1-build-$(date -u +%F+%T).log"
+if [ -L $1-build.log ]; then
+    mv -f $1-build.log $1-build-previous.log
+fi
 ln -sf $LOG_FILE lts-build.log
-time script -q -c "./build.sh $*" $LOG_FILE
+time script -c "./build.sh $*" $LOG_FILE
 touch -h lts-build.log
 date
