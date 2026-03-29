7e05ce64dc2f3fcceb41a93bffbfb85a3b0b04b2
Jens Petersen <juhpetersen@gmail.com>
2026-03-29 21:35:37 +0800

    build.sh: bump stack to 3.9.3

--- automated/build.sh
+++ automated/build.sh
@@ -83,7 +83,7 @@ rm -f curator stack -- *.bz2
 curl -L "https://github.com/commercialhaskell/curator/releases/download/commit-cc14f1d1c5877e75cfdf7e0c9c1f6b7b6a040bdd/curator.bz2" | bunzip2 > curator
 chmod +x curator
 
-STACK_VERSION=3.9.1
+STACK_VERSION=3.9.3
 # rc url
 #curl -L https://github.com/commercialhaskell/stack/releases/download/rc%2Fv${STACK_VERSION}/stack-${STACK_VERSION}-linux-x86_64-bin > stack
 curl -L https://github.com/commercialhaskell/stack/releases/download/v${STACK_VERSION}/stack-${STACK_VERSION}-linux-x86_64-bin > stack
