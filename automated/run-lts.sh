1f15d4f6c0817e49f4ecda6012e6f3a906656fe6
Jens Petersen <juhpetersen@gmail.com>
2026-09-07 13:47:59 +0800

    run-lts.sh: Usage help

--- automated/run-lts.sh
+++ automated/run-lts.sh
@@ -2,6 +2,11 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+if [ -z "$1" ];
+   echo "Usage: $0 lts-X.Y"
+   exit 1
+fi
+
 git pull
 LOG_FILE="logs/$1-build-$(date -u +%F+%T).log"
 if [ -L $1-build.log ]; then
